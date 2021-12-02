831530ceb01f8ab0954278d357fa9787a79e7dc9
GamingAstronamy <ethan-barnes@hotmail.com>
2021-12-02 08:48:14 -0600

    Initial

---
+++ app/models.py
@@ -0,0 +1,34 @@
+from app import db, login
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    color = db.Column(db.String(64), default='#ffffff')
+
+    messages = db.relationship('Message', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Message {self.content}>'
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
