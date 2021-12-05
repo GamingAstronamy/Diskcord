bea246e2823d7a35bdb99eff38f8fe4f0b45dcc5
GamingAstronamy <ethan-barnes@hotmail.com>
2021-12-05 01:09:51 -0600

    idfk

--- diskcord.py
+++ diskcord.py
@@ -4,3 +4,6 @@ from app.models import User, Message
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Message': Message}
+
+if __name__ == '__main__':
+    pass
